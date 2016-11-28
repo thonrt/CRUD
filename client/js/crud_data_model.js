dc03d9b7381947edd569b105fc631968aadfc200
Bella Duan <bella.duan@morningstar.com>
2016-11-28 18:59:45 +0800

    update

--- client/js/crud_data_model.js
+++ client/js/crud_data_model.js
@@ -1,16 +1,13 @@
 export default class CrudDataModel {
 	requestData(option, successCallback, errorCallBack) {
 		var server = "http://localhost:8000";
-		if (option.type === "GET" || option.type === "delete") {
-			option.contentType = null;
-		}
 		$.ajax({
 			url: server + option.url,
 			dataType: 'json',
 			type: option.type || "GET",
 			cache: false,
 			data: option.data || null,
-			contentType: option.contentType,
+			contentType: option.contentType || null,
 			success: successCallback,
 			error: errorCallBack
 		});
